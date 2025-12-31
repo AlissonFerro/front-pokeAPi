959f504632b57c8c6b8104f2b02804749af31eb8
Alisson Ferro <alisson_.ferro@hotmail.com>
2025-12-31 18:16:10 -0300

    feat: change to ts

---
+++ src/Abstract/favorits.ts
@@ -0,0 +1,27 @@
+export function getFavorits(): Array<string | null> {
+    const favoritsString = localStorage.getItem('favorits');
+    if (!favoritsString) {
+        localStorage.setItem('favorits', JSON.stringify([]));
+        return [];
+    }
+    const favoritsArray = JSON.parse(favoritsString);
+    return favoritsArray;
+}
+
+export function addFavorits(item): void {
+    const favorits = getFavorits();
+    if (!favorits.includes(item)) {
+        favorits.push(item);
+        localStorage.setItem('favorits', JSON.stringify(favorits));
+    } else {
+        console.log("Item já está nos favoritos.");
+    }
+}
+
+export function removeToFavorits(item): void {
+    const favorits = getFavorits();
+
+    const updatedFavorits = favorits.filter((fav) => fav !== item);
+
+    localStorage.setItem('favorits', JSON.stringify(updatedFavorits));
+}
